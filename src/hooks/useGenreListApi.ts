94fa50de46ad852923edf2593c4bc3aa807952ec
Salv1991 <Panagiotis_Salvanos@hotmail.com>
2023-09-27 16:15:49 +0300

    finished

--- src/hooks/useGenreListApi.ts
+++ src/hooks/useGenreListApi.ts
@@ -15,19 +15,15 @@ export const useGenreListApi = ( genreType:MediaType ) => {
         .then(fetchedData => {
             setData(fetchedData.genres);
             setIsLoaded(true);
-            console.error( fetchedData);
-            console.error(`https://api.themoviedb.org/3/genre/${genreType}/list?language=en-US&api_key=${import.meta.env.VITE_API_KEY_MOVIESTMDB}`);
 
         })
         .catch(err => {
-            console.error(err);
-            setError(error);
+            setError(err);
         });
     };
 
     useEffect(() => {
         fetchData();        
-        console.log("data by useApi", data);
       },[]); 
       
       return {data, isLoaded, error }
