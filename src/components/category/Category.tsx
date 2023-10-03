bbfc4e06f087c7146b8e93e45be1282a145b34a0
Salv1991 <Panagiotis_Salvanos@hotmail.com>
2023-10-03 13:36:12 +0300

    started implementing react-query

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -10,6 +10,7 @@ import { CategoryType } from '../../enums/categoryType';
 import { useApi } from '../../hooks/useApi';
 //COMPONENTS
 import LoadingSpinner from '../loadingSpinner/LoadingSpinner';
+import { useQuery } from 'react-query';
 //TYPES
 type CategoryProps = {
     url: string;
@@ -20,17 +21,30 @@ type CategoryProps = {
 
 
 const Category = ({ url, categoryType, categoryPageNumber, setCategoryPageNumber }:CategoryProps) => {
+    let totalPages= 0;
+
     const imagePathWidth154 = `https://image.tmdb.org/t/p/w154/`;
-    const {data, isLoaded, error, setCategoryPageNumber: setPage} = useApi(url, categoryPageNumber, setCategoryPageNumber);
-    let totalPages= data.total_pages;
-    if(data.total_pages>=500) {
-        totalPages = 500;
-   }
+/*     const {data, isLoaded, error} = useApi(url, categoryPageNumber);
+ */    
+ 
+    const {data, status } = useQuery(['category', url, categoryPageNumber], async () => {
+        const response = await fetch(`https://api.themoviedb.org/3/${url}&page=${categoryPageNumber}&api_key=${import.meta.env.VITE_API_KEY_MOVIESTMDB}`)        
+        return response.json();
+    });
+    
+    function totalPagesCounter () {
+        totalPages= data.total_pages;
+        if(data.total_pages>=500) {
+            totalPages = 500;
+        }
+        return totalPages;
+    }
+     
     return(
         <>
         <div  className={categoryStyles['category-wrapper']}>
             {/*LOADING */}
-            {!isLoaded && !error &&
+            {status ==='loading' && 
             <div className={categoryStyles['category-wrapper']}>
                 <div >
                     <LoadingSpinner />
@@ -39,101 +53,106 @@ const Category = ({ url, categoryType, categoryPageNumber, setCategoryPageNumber
             }
 
             {/*MOVIES CONTAINER */}
-            {data.results && isLoaded && 
-            <>
-            <div className={categoryStyles['total-pages-details']}>
-                <h3>  {data.total_results} results</h3>
-             </div>
-            <div className={categoryStyles['movies-container']}>
-                
-                {data.results.map( (movie) => (
-                    <Link
-                        reloadDocument  
-                        to={`/${categoryType}/${movie.id}`} 
-                        key={movie.id} 
-                        className={categoryStyles['movie-container']}
-                    >
-                        <div className={categoryStyles['movie-image-container']}>
-                            {movie.poster_path==null ? (
-                                <img loading='lazy' className={`${categoryStyles['placeholder-image']} ${categoryStyles['movie-image']}`} src='/images/placeholder-image.svg' alt="placeholder image" />     
-                            ):(
-                                <img loading='lazy' className={categoryStyles['movie-image']} src={`${imagePathWidth154}${movie.poster_path}`} alt='movie image' />     
-                            )
-                            }
-                        </div>
-                        <div className={categoryStyles['movie-details']}>
-                        <h3 className={categoryStyles['movie-title']}>{movie.title || movie.name || movie.original_title || movie.original_name || 'Name not found'}</h3>
-                            <p className={categoryStyles['movie-overview']}>{movie.overview===''? 'no summary available': movie.overview}</p>
-                            <div className={categoryStyles['movie-ratings']}>
-                                <h3>{movie.vote_average.toFixed(1)}</h3>
-                                <div><StarIcon/></div>
-                                <span>{`(${movie.vote_count})`}</span>
+            {status==='success'  && 
+                <>
+                <div className={categoryStyles['total-pages-details']}>
+                    <h3>  {data.total_results} results</h3>
+                </div>
+                <div className={categoryStyles['movies-container']}>
+                    
+                    {data.results.map( (movie:any) => (
+                        <Link
+                            reloadDocument  
+                            to={`/${categoryType}/${movie.id}`} 
+                            key={movie.id} 
+                            className={categoryStyles['movie-container']}
+                        >
+                            <div className={categoryStyles['movie-image-container']}>
+                                {movie.poster_path==null ? (
+                                    <img loading='lazy' className={`${categoryStyles['placeholder-image']} ${categoryStyles['movie-image']}`} src='/images/placeholder-image.svg' alt="placeholder image" />     
+                                ):(
+                                    <img loading='lazy' className={categoryStyles['movie-image']} src={`${imagePathWidth154}${movie.poster_path}`} alt='movie image' />     
+                                )
+                                }
                             </div>
-                        </div>
-                    </Link>    
-                ))}
-            </div>
+                            <div className={categoryStyles['movie-details']}>
+                            <h3 className={categoryStyles['movie-title']}>{movie.title || movie.name || movie.original_title || movie.original_name || 'Name not found'}</h3>
+                                <p className={categoryStyles['movie-overview']}>{movie.overview===''? 'no summary available': movie.overview}</p>
+                                <div className={categoryStyles['movie-ratings']}>
+                                    <h3>{movie.vote_average.toFixed(1)}</h3>
+                                    <div><StarIcon/></div>
+                                    <span>{`(${movie.vote_count})`}</span>
+                                </div>
+                            </div>
+                        </Link>    
+                    ))}
+                </div>
+                
             
-            </>
+                    
+                {/* CATEGORY PAGES NAVIGATION */}
+                
+                <div className={categoryStyles['pages-container']}>
+                    <button
+                        title='First page'
+                        onClick={()=> setCategoryPageNumber(1)}
+                    >
+                        <ChevronDoubleLeftIcon/>
+                    </button>
+
+                    <button 
+                        title='Previous page'                
+                        onClick={()=> {
+                            if(data.page<=1)return;
+                            setCategoryPageNumber(data.page-1);
+                            window.scrollTo(0, 0);
+                            }
+                        }  
+                    >
+                        <ChevronLeftIcon/>
+                    </button>  
+
+                    <span>{`${data.page}/${totalPagesCounter()}`}</span>
+
+                    <button
+                        title='Next page'   
+                        onClick={()=> {
+                            if(data.page>=totalPagesCounter())return;
+                            setCategoryPageNumber(data.page+1)
+                            window.scrollTo(0, 0)
+
+                        }}
+                    >
+                        <ChevronRightIcon/>
+                    </button>
+
+                    <button
+                        title='Last page'   
+                        onClick={()=> {  
+                        if(data.page>=totalPagesCounter())return;
+                        setCategoryPageNumber(totalPagesCounter());
+                            window.scrollTo(0, 0)
+                        }}
+                    >
+                        <ChevronDoubleRightIcon/>
+                    </button>
+                </div>
+                </>
             }
-            { !isLoaded &&  error && 
+
+            {/*ERROR */}
+            { status ==='error' &&
                 <div className={categoryStyles['no-results-container']}>
-                    <h3> Error </h3>
+                    <h3> Error fetching data </h3>
                 </div>    
             }
             
             {/*NO RESULTS */}
-            {data.results!==undefined && data.results.length===0 && isLoaded && 
+            {status==='success' && data.results.length===0 &&
                 <div className={categoryStyles['no-results-container']}>
                     <h3>NO RESULTS</h3>
                 </div>    
             }
-            {/* CATEGORY PAGES NAVIGATION */}
-            <div className={categoryStyles['pages-container']}>
-                <button
-                    title='First page'
-                    onClick={()=> setPage(1)}
-                >
-                    <ChevronDoubleLeftIcon/>
-                </button>
-
-                <button 
-                    title='Previous page'                
-                    onClick={()=> {
-                        if(data.page<=1)return;
-                        setPage(data.page-1);
-                        window.scrollTo(0, 0);
-                        }
-                    }  
-                >
-                    <ChevronLeftIcon/>
-                </button>  
-
-                <span>{`${data.page}/${totalPages}`}</span>
-
-                <button
-                    title='Next page'   
-                    onClick={()=> {
-                        if(data.page>=totalPages)return;
-                        setPage(data.page+1)
-                        window.scrollTo(0, 0)
-
-                    }}
-                >
-                    <ChevronRightIcon/>
-                </button>
-
-                <button
-                    title='Last page'   
-                    onClick={()=> {  
-                    if(data.page>=totalPages)return;
-                        setPage(totalPages);
-                        window.scrollTo(0, 0)
-                    }}
-                >
-                    <ChevronDoubleRightIcon/>
-                </button>
-            </div>
         </div>
         </>
 
--- src/layouts/rootLayout/RootLayout.tsx
+++ src/layouts/rootLayout/RootLayout.tsx
@@ -20,7 +20,6 @@ const RootLayout = ({selectedPage}:RootLayout) => {
             <main >      
                 <Outlet />
             </main>
-            <Footer />
         </>
     )
 }
